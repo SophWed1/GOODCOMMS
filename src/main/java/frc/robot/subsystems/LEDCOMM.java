9e9c172174a9666ad08a4806a7224efd4686ce52
SophWed1 <111957156+SophWed1@users.noreply.github.com>
2023-01-28 12:11:38 +1100

    binded sensor values to motors

--- src/main/java/frc/robot/subsystems/LEDCOMM.java
+++ src/main/java/frc/robot/subsystems/LEDCOMM.java
@@ -8,6 +8,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalOutput;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.RainbowAnimation;
@@ -17,12 +20,24 @@ public class LEDCOMM extends SubsystemBase {
   DigitalOutput ColourSensor;
   CANdle mCaNdle;
   RainbowAnimation mRainbowAnimation;
+  TalonFX LeftFront;
+  TalonFX DTRR;
+
   /** Creates a new LEDCOMM. */
   public LEDCOMM() {
     ProximitySensor = new DigitalInput(0);
     ColourSensor = new DigitalOutput(1);
     mCaNdle = new CANdle(1);
+    LeftFront = new TalonFX(3);
+    DTRR = new TalonFX(6);
     mRainbowAnimation = new RainbowAnimation();
+
+    LeftFront.setNeutralMode(NeutralMode.Brake);
+    DTRR.setNeutralMode(NeutralMode.Brake);
+
+    LeftFront.setInverted(true);
+    DTRR.setInverted(false);
+
   }
 
   @Override
@@ -34,6 +49,8 @@ public class LEDCOMM extends SubsystemBase {
   public boolean cubeDetection(){
     if (ProximitySensor.get() == false){
       return true;
+    } else if (ProximitySensor.get() == false && ColourSensor.get() == true) {
+      return true;
     } else {
       return false;
     }
@@ -42,6 +59,8 @@ public class LEDCOMM extends SubsystemBase {
   public boolean coneDetection(){
     if (ColourSensor.get() == true){
       return true;
+    } else if (ColourSensor.get() == true && ProximitySensor.get() == false) {
+      return true;
     } else {
       return false;
     }
@@ -49,21 +68,37 @@ public class LEDCOMM extends SubsystemBase {
 
   public void turnOnLEDS(){
     if (cubeDetection()){
-
       mCaNdle.clearAnimation(0);
-      mCaNdle.setLEDs(94, 12, 166);
+      mCaNdle.setLEDs(94, 12, 166);    
+      
+      stopLeftFront();
+      stopDTRR();
 
     } else if (coneDetection()){
 
       mCaNdle.clearAnimation(0);
       mCaNdle.setLEDs(252, 229, 28);
 
+      stopLeftFront();
+      stopDTRR();
+
     } else {
-      
+
+      LeftFront.set(TalonFXControlMode.PercentOutput, 0.4);
+      DTRR.set(TalonFXControlMode.PercentOutput, 0.4);
+
       mCaNdle.clearAnimation(0);
       //mCaNdle.animate(mRainbowAnimation);
     }
 
+  }
+
+  public void stopLeftFront(){
+    LeftFront.set(TalonFXControlMode.PercentOutput, 0);
+  }
+
+  public void stopDTRR(){
+    DTRR.set(TalonFXControlMode.PercentOutput, 0);
   }
 
 }
