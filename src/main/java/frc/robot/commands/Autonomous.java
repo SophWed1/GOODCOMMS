5f72d5fea42536ff6f04504187887ae1bbcb245e
SophWed1 <111957156+SophWed1@users.noreply.github.com>
2023-01-28 16:58:15 +1100

    commented auto strategy

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -2,6 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
+/*
+ * Auto scoring: 2x high cone nodes & 1x high cube node; steps:
+ * 1. score preload
+ * 2. get game piece (cube; first intake)
+ * 3. score game piece (cube; second score)
+ * 4. get game piece (cone; second intake)
+ * 5. score game piece (cone; third score)
+ * 6. park
+ */ 
+//TODO note: this is the auto strategy for max efficiency to be used in qualification matches or if our allies can't do anything. 
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
